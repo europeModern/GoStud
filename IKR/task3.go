f2bb947ae13a937c8fb0444eeb2a1cde1d40c5f2
europeModern <mishadyakov07@gmail.com>
2026-01-09 16:02:24 +0300

    Исправление

--- IKR/task3.go
+++ IKR/task3.go
@@ -6,41 +6,41 @@ import (
 )
 
 func encryptWord(word string) string {
-	if len(word) <= 1 {
+	runes := []rune(word)
+	if len(runes) <= 1 {
 		return word
 	}
+	firstChar := string(runes[0])
+	restRunes := runes[1:]
 	
-	firstChar := string(word[0])
-	restChars := word[1:]
-	
-	runes := []rune(restChars)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	for i, j := 0, len(restRunes)-1; i < j; i, j = i+1, j-1 {
+		restRunes[i], restRunes[j] = restRunes[j], restRunes[i]
 	}
-	
-	return firstChar + string(runes)
+
+	return firstChar + string(restRunes)
 }
 
 func encryptPhrase(phrase string) string {
 	words := strings.Fields(phrase)
 	encryptedWords := make([]string, len(words))
-	
+
 	for i, word := range words {
 		encryptedWords[i] = encryptWord(word)
 	}
-	
+
 	return strings.Join(encryptedWords, " ")
 }
 
 func main() {
 	fmt.Println("ЗАДАЧА #3: Шифратор фраз")
-	
+
 	testPhrases := []string{
 		"Pepe Schnele is a legend",
 		"Hello world",
 		"Go programming language",
 		"a",
 		"test",
+		"Привет, Мир!"
 	}
 	
 	for _, phrase := range testPhrases {
